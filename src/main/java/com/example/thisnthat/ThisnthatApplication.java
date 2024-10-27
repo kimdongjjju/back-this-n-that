d2ebf364eabd20f4ffeb961be9031e5c14d51898
kimdongju <tteangju.kim@gmail.com>
2024-10-28 01:42:22 +0900

    first commit init project

---
+++ src/main/java/com/example/thisnthat/ThisnthatApplication.java
@@ -0,0 +1,13 @@
+package com.example.thisnthat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisnthatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisnthatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thisnthat/ThisnthatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thisnthat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisnthatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
